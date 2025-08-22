b639f87f94fae8968e913c55a6b86199b0195978
ansh jain <ansh.jain@naukri.com>
2025-08-22 14:35:38 +0530

    Allow cleartext traffic for local development and add OkHttp profiler
    This commit introduces the following changes:
    
    -   Adds a network security configuration file to allow cleartext HTTP traffic for specific local IP addresses (10.0.2.2 and 192.168.x.x).
    -   Updates the AndroidManifest.xml to use this network security configuration and explicitly set `android:usesCleartextTraffic="true"`.
    -   Updates the Android Gradle Plugin version.
    -   Modifies `RetrofitInstance.kt` to:
        -   Change the `BASE_URL` to `http://10.0.2.2:3000`.
        -   Integrate `OkHttpProfilerInterceptor` for network request debugging.
    -   Adds the `okhttp-profiler` dependency to `app/build.gradle.kts`.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -91,4 +91,5 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+    implementation("io.nerdythings:okhttp-profiler:1.1.1")
 }
--- app/src/main/java/com/example/hirenow/RetrofitInstance.kt
+++ app/src/main/java/com/example/hirenow/RetrofitInstance.kt
@@ -1,14 +1,21 @@
 package com.example.hirenow
 
+import io.nerdythings.okhttp.profiler.OkHttpProfilerInterceptor
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitInstance {
-    private const val BASE_URL = "http://localhost:3000"
+    private const val BASE_URL = "http://10.0.2.2:3000"
+    val builder = OkHttpClient.Builder().addInterceptor(
+        OkHttpProfilerInterceptor()
+    )
 
+    val client = builder.build()
     private val retrofit by lazy {
         Retrofit.Builder()
             .baseUrl(BASE_URL)
+            .client(client)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
     }
